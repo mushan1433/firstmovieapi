c2938965f82719bdb790e23b87cd271d1e6e0efb
mushan1433 <124557176+mushan1433@users.noreply.github.com>
2023-06-01 12:50:00 +0600

    Update search.js

--- search.js
+++ search.js
@@ -6,7 +6,7 @@ console.log(movie);
 axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1413f426e96bd33478cc6d119de2ee0f&query=${movie}`).then(res => {
   console.log(res.data.results);
   selectmovie.innerHTML = res.data.results.map(val => {
-    return `<div class = singlemovie><a href="/singleMovie.html?id=${val.id}">
+    return `<div class = singlemovie><a href="/firstmovieapi/singleMovie.html?id=${val.id}">
                   <div class="selectmovieimg">
                          <img src="https://image.tmdb.org/t/p/w500${val.backdrop_path}">
                   </div>
