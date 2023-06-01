16d76a8cb1a8da8ab7d9a8ebafc49c77d36b38db
mushan1433 <124557176+mushan1433@users.noreply.github.com>
2023-06-01 12:51:16 +0600

    Update upcomeing.js

--- upcomeing.js
+++ upcomeing.js
@@ -12,7 +12,7 @@ axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=1413f426e96bd3347
     console.log(res.data);
     wrap.innerHTML = res.data.results.map(val => {
         return `<div class="singleMovie">
-                <a href="/singleMovie.html?id=${val.id}">
+                <a href="/firstmovieapi/singleMovie.html?id=${val.id}">
                 <img src="https://image.tmdb.org/t/p/w500${val.backdrop_path}">
                 <div class="text">
                 <h2>${val.title}</h2>
